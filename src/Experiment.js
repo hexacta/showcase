ab82f57f75653787be5011fa42e5a9a25dd3f0dd
pomber <pombopombopombo@gmail.com>
2017-03-24 16:18:10 -0300

    Move Experiment to styled-components

--- src/Experiment.js
+++ src/Experiment.js
@@ -1,7 +1,43 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import Button from "./Button";
 import ExperimentSection from "./ExperimentSection";
 import "./Experiment.css";
+
+const Container = styled.div`
+	color: #FAFAFA;
+	height: 100%;
+	padding: 20px 0;
+	margin: 0 auto;
+	max-width: 1000px;
+`;
+
+const Header = styled.header`
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 20px;
+`;
+
+const Title = styled.h1`
+	margin: 0;
+	padding: 12px;
+`;
+
+const P = styled.p`
+	margin: 0;
+	color: #aaa;
+	line-height: 1.3em;
+	padding: 12px;
+	font-size: 18px;
+`;
+
+const Nav = styled.nav`
+	min-width: 200px;
+	width: 300px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
 
 class Experiment extends Component {
   componentDidMount() {
@@ -9,34 +45,27 @@ class Experiment extends Component {
   }
 
   render() {
-    const {
-      title,
-      description,
-      video,
-      launch,
-      repo,
-      sections
-    } = this.props.info;
+    const { title, description, launch, repo, sections } = this.props.info;
     const secs = sections.map((section, i) => (
       <ExperimentSection key={i} {...section} />
     ));
     return (
-      <div className="experiment">
-        <header>
+      <Container>
+        <Header>
           <div>
-            <h1>{title}</h1>
-            <p>{description}</p>
+            <Title>{title}</Title>
+            <P>{description}</P>
           </div>
-          <nav>
+          <Nav>
             <Button href={launch} filled>Launch</Button>
             <Button href={repo}>Get The Code</Button>
-          </nav>
-        </header>
-        <div className="experiment-video landscape">
+          </Nav>
+        </Header>
+        {/*<div className="experiment-video landscape">
           <iframe src={video} frameBorder="0" allowFullScreen />
-        </div>
+        </div>*/}
         {secs}
-      </div>
+      </Container>
     );
   }
 }
