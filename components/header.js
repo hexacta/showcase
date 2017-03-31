c5ba7dee4cb16ac14ef8cdd086e9b3fbdda5cf77
pomber <pombopombopombo@gmail.com>
2017-03-31 19:06:05 -0300

    Add header

--- components/header.js
+++ components/header.js
@@ -1,8 +1,59 @@
 import Link from "next/link";
+import theme from "./style/theme";
 
 export default () => (
   <header>
-    <Link href="https://www.hexacta.com">Hexacta</Link>
-    <Link prefetch href="/">Experiments</Link>
+    <div className="content">
+      <Link href="https://www.hexacta.com">
+        <a>
+          <img className="logo" src="/static/hexacta.png" alt="Hexacta" />
+        </a>
+      </Link>
+      <nav>
+        <Link prefetch href="/"><a className="active">Experiments</a></Link>
+        <Link prefetch href="https://medium.com/hexacta"><a>Blog</a></Link>
+      </nav>
+    </div>
+
+    <style jsx>
+      {
+        `
+				header {
+	        background: ${theme.darkPrimaryColor};
+					color: ${theme.lightPrimaryColor};
+				}
+
+        .content {
+          padding: 15px;
+          max-width: ${theme.maxWidth};
+          margin: 0 auto;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+
+        .logo {
+          width: 118px;
+        }
+
+        a {
+          text-decoration: none;
+          color: inherit;
+          margin: 0 10px;
+          transition-duration: 0.2s;
+          transition-property: color;
+        }
+
+        a.active {
+          padding-bottom: 5px;
+          border-bottom: solid 2px ${theme.accentColor};
+        }
+
+        a:hover {
+          color: ${theme.lightSecondaryColor};
+        }
+				`
+      }
+    </style>
   </header>
 );
---
+++ components/style/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  // Colors
+  accentColor: "#0aac8e",
+  lightAccentColor: "#57F9DB",
+  darkBackgroundColor: "#212121",
+  darkPrimaryColor: "#212121",
+  darkSecondaryColor: "#737373",
+  lightBackgroundColor: "#f1f3f5",
+  lightPrimaryColor: "#f1f3f5",
+  lightSecondaryColor: "#bcbcbc",
+  // Sizes
+  maxWidth: "1000px"
+};
+
+export default theme;
