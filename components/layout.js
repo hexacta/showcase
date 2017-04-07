354545d8cbc76f91d94f932b8887db74bcacf135
pomber <pombopombopombo@gmail.com>
2017-04-07 16:18:07 -0300

    Fix favicon

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,7 @@ export default ({ children, title }) => (
       <title>{`Hexacta - ${title}`}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <link rel="icon" href="/static/favicon.png"/>
     </Head>
 
     <Header />
