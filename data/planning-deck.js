f31ac6fa8b0e5a172adee19a050d460aba1eaf47
pomber <pombopombopombo@gmail.com>
2017-06-01 11:47:45 -0300

    Update planning deck content

--- data/planning-deck.js
+++ data/planning-deck.js
@@ -8,15 +8,25 @@ export default {
   repo: "https://github.com/hexacta/planning-deck",
   tags: ["Polymer", "Mobile", "PWA", "Firebase"],
   sections: [
+    // {
+    //   title: "Performant Dragging Animations",
+    //   description: "Animations should be rendered at 60fps, that makes fluent dragging animations a challenge to the developer, because the animation depends on the real-time user interaction and you have less than 16ms to draw the new frame.",
+    //   img: base + "planning-deck/performant.gif",
+    //   blog: {
+    //     title: "Tips for dragging animations",
+    //     url: "https://medium.com/",
+    //     author: "Rodrigo Pombo",
+    //     avatar: "https://cdn-images-1.medium.com/fit/c/32/32/1*lJ3fWONmPl2rX1QMolsanA.jpeg"
+    //   }
+    // },
     {
-      title: "Performant Dragging Animations",
-      description: "Animations should be rendered at 60fps, that makes fluent dragging animations a challenge to the developer, because the animation depends on the real-time user interaction and you have less than 16ms to draw the new frame.",
-      img: base + "planning-deck/performant.gif",
-      blog: {
-        title: "Tips for dragging animations",
-        url: "https://medium.com/",
-        author: "Rodrigo Pombo",
-        avatar: "https://cdn-images-1.medium.com/fit/c/32/32/1*lJ3fWONmPl2rX1QMolsanA.jpeg"
+      title: "Progressive Web App",
+      description: "Planning Deck scores 87/100 on Lighthouse PWA test. The test validates PWA best practices and performance. This app includes good practices like: custom splash screen, offline support, mobile-friendly design, fast page load, etc.",
+      img: base + "planning-deck/lighthouse.png",
+      link: {
+        title: "Lighthouse Report",
+        url: "https://googlechrome.github.io/lighthouse/viewer/?gist=ee2003a2556b17e22b102f835d00db14",
+        img: base + "planning-deck/score.png"
       }
     },
     {
